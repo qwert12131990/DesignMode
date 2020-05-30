211094ae475eb5c03171a962d1d12aa5ba439c41
luchao <371551824@qq.com>
2020-05-30 17:41:55 +0800

    Create code2.cpp

---
+++ OJTst/有效数字/code2.cpp
@@ -0,0 +1,124 @@
+class Solution {
+public:
+    string& trim(string &s) 
+    {
+        if (s.empty()) 
+        {
+            return s;
+        }
+        s.erase(0,s.find_first_not_of(" "));
+        s.erase(s.find_last_not_of(" ") + 1);
+        return s;
+    }
+
+    bool isNumber(string s) {
+        string str=s;
+        str=trim(str);
+
+        if (str.size()==0)
+        {
+            return false;
+        }
+        return test(str);
+    }
+
+    bool test(string& str)
+    {
+        int len=str.size();
+        bool sflag=false;
+        bool decflag=false;
+        bool eflag=false;
+        bool nflag=false;
+        int status=1;
+        /*
+            0: +-
+            1: cmp
+            2: .
+            3: e
+         */
+        
+        for(int i=0;i<len;)
+        {
+            switch(status)
+            {
+                case 0:
+                {
+                    if ((eflag==true && sflag==false)  || i==0)
+                    {
+                        i++;
+                        sflag=true;
+                        status=1;
+                        break;
+                    }
+                    return false;
+                }
+                case 2:
+                {
+                    if (decflag == false && eflag == false)
+                    {
+                        status=1;
+                        i++;
+                        decflag=true;
+                        break;      
+                    }
+                    return false;
+                }
+                case 3:
+                {
+                    if (eflag == false && nflag==true)
+                    {
+                        status=1;
+                        i++;
+                        eflag=true;
+                        nflag=false;
+                        sflag=false;
+                        break;
+                    }
+                    return false;
+                }
+                case 1:
+                {
+                    if (((unsigned int)(str.at(i)-'0'))<=9)
+                    {
+                        i++;
+                        nflag=true;
+                        sflag=true;
+                        break;
+                    }
+                    switch(str.at(i))
+                    {
+                        case '+':
+                        case '-':
+                        {
+                            status=0;
+                            break;
+                        }
+                        case '.':
+                        {
+                            status=2;
+                            break;
+                        }
+                        case 'e':
+                        {
+                            status=3;
+                            break;
+                        }
+                        default:
+                            return false;
+                    }                    
+                }
+            }
+        }
+
+        if (eflag==true && nflag==false)
+        {
+            return false;
+        }
+        if (decflag==true && nflag==false)
+        {
+            return false;
+        }
+
+        return true;
+    }
+};
