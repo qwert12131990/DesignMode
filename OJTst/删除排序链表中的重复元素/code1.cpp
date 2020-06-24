ddf03389754c36b6a78238c45c837780cb5de507
luchao <371551824@qq.com>
2020-06-24 11:09:11 +0800

    Update code1.cpp

--- OJTst/删除排序链表中的重复元素/code1.cpp
+++ OJTst/删除排序链表中的重复元素/code1.cpp
@@ -1,1 +1,37 @@
-1
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* ptmp=head;
+        ListNode* pre=head;
+
+        if (!ptmp)
+        {
+            return head;
+        }
+
+        ptmp=ptmp->next;
+
+        while(ptmp)
+        {
+            if (ptmp->val == pre->val)
+            {
+                pre->next=ptmp->next;
+            }
+            else
+            {
+                pre=ptmp;
+            }
+            ptmp=ptmp->next;
+        }
+
+        return head;
+    }
+};
