b010e2d3d2eb578d19355b3d47667a2cb0208593
luchao <371551824@qq.com>
2020-06-24 14:28:56 +0800

    Update code1.cpp

--- OJTst/相同的树/code1.cpp
+++ OJTst/相同的树/code1.cpp
@@ -1,1 +1,27 @@
-1
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p && q)
+        {
+            bool condition = true;
+            condition = condition && isSameTree(p->left,q->left);
+            condition = condition && (p->val == q->val);
+            condition = condition && isSameTree(p->right,q->right);
+            return condition;
+        }
+        if (!p && !q)
+        {
+            return true;
+        }
+        return false;
+    }
+};
