a867de901c5fd9846fd37fb8b2de53f8254d7036
luchao <371551824@qq.com>
2020-07-16 20:49:12 +0800

    Update code1.cpp

--- OJTst/二叉树的锯齿形层次遍历/code1.cpp
+++ OJTst/二叉树的锯齿形层次遍历/code1.cpp
@@ -1,1 +1,55 @@
-123
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (root == nullptr) {
+            return result;
+        }
+        
+        queue<TreeNode*> unvisited;
+        unvisited.push(root);
+        vector<int> ivec; 
+        bool odd=true;
+
+        for (TreeNode *p = root; unvisited.size();) {        
+            int sz = unvisited.size();
+            ivec.resize(sz);
+            for (int idx = 0; idx != sz; ++idx) {
+                p = unvisited.front();
+                if (odd)
+                {
+                    ivec[idx] = p->val;
+                }
+                else
+                {
+                    ivec[sz-idx-1] = p->val;
+                }
+                
+
+                if (p->left) {
+                    unvisited.push(p->left);
+                }
+                if (p->right) {
+                    unvisited.push(p->right);
+                }
+
+                unvisited.pop();
+            }
+
+            odd = !odd;
+
+            result.push_back(move(ivec));
+        }
+
+        return result;
+    }
+};
