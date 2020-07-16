566b5db5089d0a09a93b16a05f767018636d7bb2
luchao <371551824@qq.com>
2020-07-16 14:41:21 +0800

    Update code1.cpp

--- OJTst/对称二叉树/code1.cpp
+++ OJTst/对称二叉树/code1.cpp
@@ -1,40 +1,41 @@
-tion for a binary tree node.
+/**
+ * Definition for a binary tree node.
  * struct TreeNode {
-   *  *     int val;
-   *   *     TreeNode *left;
-   *    *     TreeNode *right;
-   *     *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-   *      * };
-*       */
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
-  public:
+public:
     bool isSymmetric(TreeNode* root) {
-            if (!root)
-            {
-                        return true;
-                    }
-            return isSymmetricDetail(root->left, root->right);
+        if (!root)
+        {
+            return true;
         }
+        return isSymmetricDetail(root->left, root->right);
+    }
 
     bool isSymmetricDetail(TreeNode *left, TreeNode * right)
     {
-            bool condition = true;
-    
-            if (left == right)
-            {
-                        return true;
-                    } 
-            
-            if (!left || !right )
-            {
-                        return false;
-                    }
-    
-            if (left->val != right->val)
-            {
-                        return false;
-                    }
-    
-            return isSymmetricDetail(left->right, right->left) && isSymmetricDetail(left->left, right->right);
+        bool condition = true;
+
+        if (left == right)
+        {
+            return true;
+        } 
+        
+        if (!left || !right )
+        {
+            return false;
         }
+
+        if (left->val != right->val)
+        {
+            return false;
+        }
+
+        return isSymmetricDetail(left->right, right->left) && isSymmetricDetail(left->left, right->right);
+    }
 };
