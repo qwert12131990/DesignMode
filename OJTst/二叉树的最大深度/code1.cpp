982577f9f6bb7e046edaeb494e25a0d7104ab40d
luchao <371551824@qq.com>
2020-07-17 09:42:46 +0800

    Update code1.cpp

--- OJTst/二叉树的最大深度/code1.cpp
+++ OJTst/二叉树的最大深度/code1.cpp
@@ -1,1 +1,33 @@
-123
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root)
+        {
+            return 0;
+        }
+        return maxDepth_detail(root, 1);
+    }
+
+    int maxDepth_detail(TreeNode *root, int i)
+    {
+        int left = i;
+        int right = i;
+
+        if (root->left)
+            left = maxDepth_detail(root->left, i+1);
+
+        if (root->right)
+            right = maxDepth_detail(root->right, i+1);
+        
+        return left>right?left:right;
+    }
+};
