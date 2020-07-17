eb06e765ca7048f2a4875883931c938bdabf8e4f
luchao <371551824@qq.com>
2020-07-17 16:14:30 +0800

    Update code1.cpp

--- OJTst/搜索插入位置/code1.cpp
+++ OJTst/搜索插入位置/code1.cpp
@@ -1,1 +1,8 @@
-123
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int i=0;
+        for(; i<nums.size() && nums[i]<target; i++) {}
+        return i;
+    }
+};
